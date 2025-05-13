fcf4aa5ce0b9bc69ea1ad033cbc7eb4a80f3405d
Artturi Euren <artturi.kinnunen@hotmail.fi>
2025-05-13 21:13:27 +0300

    login.sh first version

---
+++ postgres/login.sh
@@ -0,0 +1,29 @@
+# Main menu function
+main_menu() {
+
+    echo "Choose an option:"
+    echo "1) Login to container"
+    echo "2) Login to DB"
+    read -p "Enter your choice [1 or 2]: " choice
+
+    case "$choice" in
+    1) login_container ;;
+    2) login_db ;;
+    esac
+}
+# Login to container function
+login_container() {
+    container=$(docker ps | grep postgres | awk '{ print $1 }')
+    docker exec -it $container /bin/bash
+}
+# Login to DB function
+login_db() {
+    PG_USERNAME=$(cat files/secret | awk '{ print $1 }')
+    PGPASSWORD=$(cat files/secret | awk '{ print $2 }')
+    echo "Connecting to database ... "
+    echo "$PG_USERNAME"
+    psql postgresql://$PG_USERNAME:$PGPASSWORD@127.0.0.1:5432
+}
+
+# Hey, lets start
+main_menu
