c9c23c1163e95331fcbabce3b84d094a7e8d5da0
Artturi Euren <artturi.kinnunen@hotmail.fi>
2025-05-14 20:42:46 +0300

    stop container + docker image removal, take it from install.sh

--- postgres/stop.sh
+++ postgres/stop.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Get the container ID of the running Postgres container
-container=$(docker ps | grep postgres | awk '{ print $1 }')
+IMAGE_NAME=$(cat install.sh | grep IMAGE_NAME= |grep -oP '(?<=")[^"]+(?=")')
+container=$(docker ps -all | grep postgres | awk '{ print $1 }')
 
 # Check if a container was found
 if [ -n "$container" ]; then
   docker stop "$container"
   docker rm "$container"
   echo "Postgres container stopped and removed. Running confirmation:"
+  docker ps | grep postgres
+  docker image rm $IMAGE_NAME
+  echo "Docker image removed"
 else
   echo "No running Postgres container found."
+  docker ps | grep postgres
 fi
-
-# Show any remaining Postgres containers (should be none)
-docker ps | grep postgres
